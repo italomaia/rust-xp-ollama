08dd35eb6b04801f5fb88df0815422d4403b862d
Italo Maia <italo.maia@gmail.com>
2024-02-25 00:12:00 +0100

    chore(c02): save context for debug

--- examples/02-context.rs
+++ examples/02-context.rs
@@ -1,6 +1,7 @@
 use ollama_rs::generation::completion::GenerationContext;
 use ollama_rs::generation::completion::request::GenerationRequest;
 use ollama_rs::Ollama;
+use simple_fs::save_json;
 use xp_ollama::consts::MODEL;
 use xp_ollama::gen::gen_stream_print;
 use xp_ollama::Result;
@@ -31,6 +32,10 @@ async fn main() -> Result<()> {
 
         if let Some(final_data) = final_data {
             last_ctx = Some(final_data.context);
+
+            // save for debug
+            let ctx_file_path = "data/c02-ctx.json";
+            let _ = save_json(ctx_file_path, &last_ctx);
         }
     }
 
